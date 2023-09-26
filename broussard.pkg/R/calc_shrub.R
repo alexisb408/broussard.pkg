49340cc222d17608e115d3b5b7238e962095c7cf
Alexis Broussard <alexis.broussard-2@selu.edu>
2023-09-26 18:56:45 +0000

    look at my clever modification to shrub density

--- broussard.pkg/R/calc_shrub.R
+++ broussard.pkg/R/calc_shrub.R
@@ -2,5 +2,5 @@ calc_den <- function(length, width, height){
   volume <- calc_vol(length, width, height)
   mass <- calc_mass(length)
   density <- mass/volume
-  return(density)
+  return(densty)
 }
